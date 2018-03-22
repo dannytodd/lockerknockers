e580f08a7f3cd6b447c0897b6040f7b8ffa86082
Danny Todd <Dannyt96@hotmail.co.uk>
2018-03-22 15:04:20 +0000

    set up states, store button and buzzer. as well as barebones code structure for knock storing/comparing

--- Locker_Knockers.ino
+++ Locker_Knockers.ino
@@ -1,14 +1,24 @@
+// TODO
+// - record time with distance sensor
+// - add monitor
+
+// MONITOR PIN
 unsigned int EchoPin = 2;
 unsigned int TrigPin = 3;
-const int switchPin = 7; // magnet
-const int buzzer = 9; //buzzer to arduino pin 9
-const int bluePin = 11; // storing knock
-const int redPin = 12; // failed knock attempt
-const int greenPin = 13; // successful knock attempt
+const int StorePin = 5;
+const int SwitchPin = 7; // magnet
+const int Buzzer = 9; //buzzer to arduino pin 9
+
+const int BluePin = 11; // storing knock
+const int RedPin = 12; // failed knock attempt
+const int GreenPin = 13; // successful knock attempt
 
 bool Successful_Knock = false;
 bool Door_Open = false;
-int Door_Data[100] = {};
+float Stored_Knock[100] = {};
+float Temp_Knock[10] = {};
+int Temp_Knock_Count = 0;
+bool Attempt_Finished = false;
 
 unsigned long Time_Echo_us = 0;
 //Len_mm_X100 = length*100
@@ -21,9 +31,62 @@ void setup()
   Serial.begin(9600);
   pinMode(EchoPin, INPUT);
   pinMode(TrigPin, OUTPUT);
-  pinMode(switchPin, INPUT);
-  //digitalWrite(switchPin, HIGH); // fucks up magnet reading as only HIGH even though it is in tutorial
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(StorePin, INPUT);
+  pinMode(SwitchPin, INPUT);
+  pinMode(Buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+  pinMode(BluePin, OUTPUT);
+  pinMode(RedPin, OUTPUT);
+  pinMode(GreenPin, OUTPUT);
+
+  digitalWrite(SwitchPin, HIGH);
+}
+
+void resetTempKnock(){
+  Serial.println("RESET TEMP KNOCK");
+  // sizeof(Temp_Knock) TODO
+  for( int i = 0; i < 10;  ++i ){
+   Temp_Knock[i] = (char)0;    
+  }
+
+  Temp_Knock_Count = 0;
+}
+
+float readDistanceData(unsigned long Time_Echo_us){
+  //Serial.println("READ DISTANCE DATA");
+  Len_mm_X100 = (Time_Echo_us * 34) / 2;
+    Len_Integer = Len_mm_X100 / 100;
+    Len_Fraction = Len_mm_X100 % 100;
+    if (Len_Integer > 200)
+    {
+      Len_Integer = 200;
+    }
+    return Len_Integer;
+}
+
+void resetState(){
+  digitalWrite(GreenPin, LOW);
+  digitalWrite(RedPin, LOW);
+  digitalWrite(BluePin, LOW);  
+  noTone(Buzzer); // stop sound
+}
+
+void setState(String state){
+  resetState();
+  Serial.println(state);
+ 
+  if (state == "SUCCESS"){
+    digitalWrite(GreenPin, HIGH);
+    delay(600);
+  }
+  else if (state == "FAIL"){
+    digitalWrite(RedPin, HIGH);
+    tone(Buzzer, 100); // send 1KHz sound signal...
+    delay(200);        // for 2 secONDS
+  }
+  else if (state == "READING"){
+    digitalWrite(BluePin, HIGH);
+    delay(500);
+  }
 }
 
 void loop()
@@ -31,38 +94,51 @@ void loop()
   digitalWrite(TrigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(TrigPin, LOW);
-
   Time_Echo_us = pulseIn(EchoPin, HIGH);
   if ((Time_Echo_us < 60000) && (Time_Echo_us > 1))
   {
-    setDoor();
-    
-    Len_mm_X100 = (Time_Echo_us * 34) / 2;
-    Len_Integer = Len_mm_X100 / 100;
-    Len_Fraction = Len_mm_X100 % 100;
-    if (Len_Integer > 200)
-    {
-      Len_Integer = 200;
-    }
+    float distance = readDistanceData(Time_Echo_us);
 
-    if (digitalRead(switchPin) == HIGH) {
-      if (Successful_Knock or Len_Integer < 100) {
-        // SUCCESS
-        digitalWrite(greenPin, HIGH);
+    if (distance < 150){
+      if (Attempt_Finished) {
+        resetTempKnock();
       }
       else {
-        // FAIL
-        digitalWrite(redPin, HIGH);
-        tone(buzzer, 100); // Send 1KHz sound signal...
-        delay(200);        // ...for 1 sec
+        Temp_Knock[Temp_Knock_Count] = distance;
+        Temp_Knock_Count += 1;
+        // http://andybrown.me.uk/2011/01/15/the-standard-template-library-stl-for-avr-with-c-streams/
+      }
+    }
+  
+    // Store button pressed
+    if (digitalRead(StorePin) == HIGH) {
+      setState("READING");
+//      knock = readKnock();
+//      if (knockAttempt(knock)) {
+//        // print NEW KNOCK
+//        newKnock = readKnock();
+//        storeKnock(newKnock);
+//      }
+//      else {
+//        // print KNOCK ATTEMPT FAIL
+//        digitalWrite(RedPin, HIGH);
+//      }
+      
+    }
+    // Door open
+    if (digitalRead(SwitchPin) == HIGH) {
+      if (Successful_Knock or distance < 100) {
+        setState("SUCCESS");
+      }
+      else {
+        setState("FAIL");
       }
     }
     else {
-      digitalWrite(switchPin, HIGH);
+      resetState();
     }
-    noTone(buzzer);     // Stop sound...
     //Serial.print("Present Length is: ");
-    //Serial.println(Len_Integer);
+    //Serial.println(distance);
     //Serial.print(".");
     //if(Len_Fraction < 10)
     //Serial.print("0");
@@ -74,8 +150,56 @@ void loop()
   delay(10);
 }
 
-void setDoor()
+
+
+void readKnock()
 {
+  float knock[] = {};
   
 }
+
+//void storeKnock()
+//{
+//  int knock = readKnock()
+//  processedKnock = processKnock(knock)
+//  
+//  Stored_Knock = processed_knock
+//}
+//
+//int[] processKnock(int rawKnock)
+//{
+//  int[] knockTroughTimes = getKnockTroughs(rawKnock)
+//  abstractKnocks = abstractifyKnocks(knockTroughTimes) 
+//}
+//
+//int[] getKnockTroughs(int knock)
+//{
+//  // returns a list of lower bounds of knock distances by their timestamp  
+//}
+//
+//int[] abstractifyKnocks(int knock)
+//{
+//  // return list of abstract multipliers for each knock e.g. [1, 1.243, 0.9435, 1.5435]
+//  //                                                          ^ 1 because the first knock is compared to by the rest
+//}
+//
+//void knockAttempt()
+//{
+//  int knock = 0
+// if (compareKnockToStored(knock)){
+//  SUCCESS 
+// }
+// else{
+//  FAIL
+// }
+//}
+//
+//bool compareKnockToStored(int knock)
+//{
+//  if(checkStoredKnockLimit()) // limit of literal times
+//  
+//  return true
+//}
+
+
 
